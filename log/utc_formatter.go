a5c5d5bb04cf3c621ea74d836c6de552c82f9e99
Sahil Muthoo <sahil.muthoo@gmail.com>
2018-04-22 19:51:51 +0100

    Always log in UTC.

---
+++ log/utc_formatter.go
@@ -0,0 +1,12 @@
+package log
+
+import "github.com/sirupsen/logrus"
+
+type UTCFormatter struct {
+	logrus.Formatter
+}
+
+func (u UTCFormatter) Format(e *logrus.Entry) ([]byte, error) {
+	e.Time = e.Time.UTC()
+	return u.Formatter.Format(e)
+}
--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"github.com/pkg/errors"
 	"github.com/sahilm/go-twitter/app"
+	"github.com/sahilm/go-twitter/log"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	logrus.SetFormatter(log.UTCFormatter{Formatter: &logrus.TextFormatter{}})
 	a, err := app.New("twitter", app.NewRouter())
 	if err != nil {
 		logrus.Fatal(errors.Wrap(err, "failed to create application"))
